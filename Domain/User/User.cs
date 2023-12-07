9297d27b9976887da7ee6a7fe2328665118b94be
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2023-12-07 14:36:07 +0200

    EditUserCLass

--- Domain/User/User.cs
+++ Domain/User/User.cs
@@ -10,8 +10,12 @@ namespace Domain.User
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string UserName { get; set; }
         public UserRole Role { get; set; } = UserRole.Customer;
         public string? Token { get; set; }
+        public Address? Billing { get; set; }
+        public Address? Shipping { get; set; }
     }
 }
