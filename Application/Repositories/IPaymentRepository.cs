6dc4c1b94296e86aa077c6f294b81bd857682c8a
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2023-12-07 14:17:30 +0200

    Feture:PayemntServiceInformation APIs

---
+++ Application/Repositories/IPaymentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Payment;
+
+namespace Application.Repositories
+{
+    public interface IPaymentRepository
+    {
+        public bool AddPaymentGateWay(PaymentGateWay paymentGateWay);
+        public PaymentGateWay Update(PaymentGateWay paymentGateWay);
+        public PaymentGateWay Retrive(string id);
+        public Task<List<PaymentGateWay>> RetriveAllAsync();
+    }
+}
---
+++ Domain/Payment/PaymentGateWay.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Payment
+{
+    public class PaymentGateWay
+    {
+        [Key]
+        public string? Id { get; set; }
+        public string? title { get; set; }
+    }
+}
