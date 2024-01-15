2d83c937a401f1063714ab9a2cebff5075304d7f
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2024-01-15 07:34:19 +0200

    Feature: creating Mailing System

---
+++ HardWherePresenter/Controllers/MailController.cs
@@ -0,0 +1,25 @@
+﻿using Application.DTOs;
+using Application.Services___Repositores.Mail;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HardWherePresenter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+
+        public MailController(IMailService _MailService)
+        {
+            _mailService = _MailService;
+        }
+
+        [HttpPost]
+        [Route("SendMail")]
+        public bool SendMail(MailData mailData)
+        {
+            return _mailService.SendMail(mailData);
+        }
+    }
+}
